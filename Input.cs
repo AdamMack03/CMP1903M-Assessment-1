48843195e7063faa4c4e805fba0001616cfdd463
AdamMack03 <102479375+AdamMack03@users.noreply.github.com>
2022-03-27 21:48:06 +0100

    Add files via upload

--- Input.cs
+++ Input.cs
@@ -9,15 +9,27 @@ namespace CMP1903M_Assessment_1_Base_Code
     public class Input
     {
         //Handles the text input for Assessment 1
-        string text = "nothing";
-        
+        string text;
+
         //Method: manualTextInput
         //Arguments: none
         //Returns: string
         //Gets text input from the keyboard
         public string manualTextInput()
         {
-
+            while (true)
+            {
+                text = Console.ReadLine();
+                if (text == "")
+                {
+                    Console.WriteLine("Looks like you forgot to type what you wanted to have analysed.\nFeel free to try again.");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
             return text;
         }
 
@@ -27,9 +39,19 @@ namespace CMP1903M_Assessment_1_Base_Code
         //Gets text input from a .txt file
         public string fileTextInput(string fileName)
         {
-
-            return text;
+            try
+            {
+                string text;
+                text = System.IO.File.ReadAllText(fileName);
+                return text;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be found.");
+                Console.WriteLine("Re-enter file name and make sure the file is in the right location.");
+                return null;
+            }
+            return null;
         }
-
     }
 }
