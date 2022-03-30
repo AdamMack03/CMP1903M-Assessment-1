dad3f96740b7e7b9bc507851baf9915421a56154
AdamMack03 <102479375+AdamMack03@users.noreply.github.com>
2022-03-30 14:27:08 +0100

    Add files via upload

---
+++ CMP1903M Assessment 1 Base Code/Analyse.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Analyse
+    {
+        //Handles the analysis of text
+
+        //Method: analyseText
+        //Arguments: string
+        //Returns: list of integers
+        //Calculates and returns an analysis of the text
+        public List<int> analyseText(string input)
+        {
+            string longWords = input;
+
+            //List of integers to hold the first five measurements:
+            //1. Number of sentences
+            //2. Number of vowels
+            //3. Number of consonants
+            //4. Number of letters
+            //5. Number of upper case letters
+            //6. Number of lower case letters
+            List<int> values = new List<int>();
+            //Initialise all the values in the list to '0'
+            for(int i = 0; i<6; i++)
+            {
+                values.Add(0);
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                values[3]++;
+                //Counts sentences
+                if (input[i] == '.' || input[i] == '!' || input[i] == '?')
+                {
+                    values[0]++;
+                }
+            //Counts vowels
+                else if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'U')
+                {
+                    values[1]++;
+                    //values[3]++;
+                    if (input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'U')
+                    {
+                        values[4]++;
+                    }
+                    else
+                    {
+                        values[5]++;
+                    }
+                }
+            //Counts consonants
+                else if (input[i] >= 'a' && input[i] <= 'z' || input[i] >= 'A' && input[i] <= 'Z')
+                {
+                    values[2]++;
+                    //values[3]++;
+
+                    if (input[i] >= 'A' && input[i] <= 'Z')
+                    {
+                        values[4]++;
+                    }
+                    else
+                    {
+                        values[5]++;
+                    }
+                }
+            }
+            if (input[input.Length - 1] == '*')
+            {
+                values[3]--;
+            }
+            return values;
+        }
+    }
+}
